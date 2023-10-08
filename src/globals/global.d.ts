11b8eedb859145e6c29c9063b74dca42bae46eb8
mohiit1502 <mohiit1502@gmail.com>
2023-10-09 01:38:11 +0530

    misc

--- src/globals/global.d.ts
+++ src/globals/global.d.ts
@@ -1,9 +1,12 @@
+import { ARMCO_CONFIG } from "@armco/node-starter-kit/types/config.interface";
+
 /* eslint-disable no-var */
 export { };
 
 declare global {
 	var logger: any;
 	var ARMCOSTATIC: {
+		config?: ARMCO_CONFIG
 		appConfig?: {
 			default_stuff?: Array<unknown>;
 			username?: string;
